0cd519be53f1a913e37cd12f80c1d3e4436fb14d
Thomas <47thommy@gmail.com>
2023-04-12 13:26:10 +0300

    Completed

---
+++ src/dummyData.js
@@ -0,0 +1,241 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 4000,
+  },
+  {
+    name: "Feb",
+    "Active User": 3000,
+  },
+  {
+    name: "Mar",
+    "Active User": 5000,
+  },
+  {
+    name: "Apr",
+    "Active User": 4000,
+  },
+  {
+    name: "May",
+    "Active User": 3000,
+  },
+  {
+    name: "Jun",
+    "Active User": 2000,
+  },
+  {
+    name: "Jul",
+    "Active User": 4000,
+  },
+  {
+    name: "Agu",
+    "Active User": 3000,
+  },
+  {
+    name: "Sep",
+    "Active User": 4000,
+  },
+  {
+    name: "Oct",
+    "Active User": 1000,
+  },
+  {
+    name: "Nov",
+    "Active User": 4000,
+  },
+  {
+    name: "Dec",
+    "Active User": 3000,
+  },
+];
+
+export const productData = [
+  {
+    name: "Jan",
+    Sales: 4000,
+  },
+  {
+    name: "Feb",
+    Sales: 3000,
+  },
+  {
+    name: "Mar",
+    Sales: 5000,
+  },
+];
+
+export const userRows = [
+  {
+    id: 1,
+    username: "Thomas",
+    avatar:
+      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "example@gmail.com",
+    status: "active",
+    transaction: "120000.00Birr",
+  },
+  {
+    id: 2,
+    username: "Abrham",
+    avatar:
+      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "example@gmail.com",
+    status: "active",
+    transaction: "350120.00Birr",
+  },
+  {
+    id: 3,
+    username: "Yoseph",
+    avatar:
+      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "example@gmail.com",
+    status: "active",
+    transaction: "5000000.00Birr",
+  },
+  {
+    id: 4,
+    username: "Yonas",
+    avatar:
+      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "example@gmail.com",
+    status: "active",
+    transaction: "850000.00Birr",
+  },
+  {
+    id: 5,
+    username: "Geleta",
+    avatar:
+      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "example@gmail.com",
+    status: "active",
+    transaction: "48260000.00Birr",
+  },
+  {
+    id: 6,
+    username: "Yared",
+    avatar:
+      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "example@gmail.com",
+    status: "active",
+    transaction: "42000.00Birr",
+  },
+  {
+    id: 7,
+    username: "Sahib",
+    avatar:
+      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "example@gmail.com",
+    status: "active",
+    transaction: "7584000.00Birr",
+  },
+  {
+    id: 8,
+    username: "Kena",
+    avatar:
+      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "example@gmail.com",
+    status: "active",
+    transaction: "500000.Birr",
+  },
+  {
+    id: 9,
+    username: "Nahom",
+    avatar:
+      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "example@gmail.com",
+    status: "active",
+    transaction: "28400000.00Birr",
+  },
+  {
+    id: 10,
+    username: "Jon Snow",
+    avatar:
+      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "example@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+];
+
+export const productRows = [
+  {
+    id: 1,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 2,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 3,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 4,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 5,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 6,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 7,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 8,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 9,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 10,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+];
